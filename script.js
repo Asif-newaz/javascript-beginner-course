4fc424b0b87ee8e619a9cddbbdc0d07be95fed51
Asif-newaz <max.asif24@gmail.com>
2020-05-15 01:01:11 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('hello');
+
+//Variables
+var b = 'smoothie';
+
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+document.getElementById('someText').innerHTML = 'Hey There';
