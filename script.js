03caff8b2c1c9d5e88797a23062f6bb76762e5f3
Asif-newaz <max.asif24@gmail.com>
2020-05-15 18:03:10 +0600

    Worked with numbers

--- script.js
+++ script.js
@@ -3,15 +3,33 @@ console.log('hello');
 // alert('Hey, whats up?');
 
 //Variables
-var b = 'smoothie';
+// var b = 'smoothie';
+// console.log(b);
 
-console.log(b);
-
-var someNumber = 45;
-console.log(someNumber);
+// var someNumber = 45;
+// console.log(someNumber);
 
 // document.getElementById('someText').innerHTML = 'Hey There';
 
+/*
 var age = prompt('What is your age?');
 
 document.getElementById('personAge').innerHTML = age;
+*/
+
+//Numbers in JavaScript 
+// var num1 = 5.7;
+// console.log(5 * 10);
+
+// var num1 = 10;
+// num1 = num1 + 1;
+// console.log(num1);
+
+var num1 = 10;
+
+//Increament num1 by 1
+// num1++;
+
+//Decreament num1 by 1
+// num1--;
+console.log(num1);
